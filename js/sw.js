b4d97cdc0b4fe6fcb9b7e24809c89f88583fc55c
TudorDan <tudor.ist@gmail.com>
2018-08-02 19:19:49 +0300

    feat: add cache worker file - fetch event

--- js/sw.js
+++ js/sw.js
@@ -11,3 +11,11 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
