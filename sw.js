5a3092729b2aadf822b6360bb3cfb96b2968b2cc
TudorDan <tudor.ist@gmail.com>
2018-08-02 22:33:01 +0300

    fix: add more resources to initial cache

--- sw.js
+++ sw.js
@@ -2,11 +2,24 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('rest-rev-app-v1').then(function(cache) {
       return cache.addAll([
-        '/',
-        'js/',
-        'css/',
-        'img/',
-        'data/'
+        './',
+        './restaurant.html',
+        './index.html',
+        './css/styles.css',
+        './data/restaurants.json',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg',
+        './js/dbhelper.js',
+        './js/main.js',
+        './js/restaurant_info.js'
       ]);
     })
   );
